66f6400758497099d59d0bb178a281f938e4dc40
Yuri de Wit <yuridewitherran@gmail.com>
2022-02-16 12:30:25 -0300

    centerd robot, still buggy

--- Mecatronica/SimulationSetup.m
+++ Mecatronica/SimulationSetup.m
@@ -4,7 +4,7 @@ run('Simulador_DataFile.m');
 %% Enviorment varialbles
 % Ground Variables
 yGridVector = [-.5 .5];
-xGridVector = [0 1.5 1.501 2 2.001 2.5 2.501 3 3.001 3.5 3.501 4 4.001 5];
+xGridVector = [0 1.5 1.501 2 2.001 2.5 2.501 3 3.001 3.5 3.501 4 4.001 5] - 0.2;
 stepVector = [.015 .015; .015 .015; .015 .015; .015 .015;];
 stepType = 1;
 switch stepType
