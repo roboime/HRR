f08b55e8a7c3866aba2f6284412b9edb875723f4
Yuri de Wit <yuridewitherran@gmail.com>
2022-02-16 12:24:22 -0300

    simple robot implementation, still buggy

--- Mecatronica/SimulationSetup.m
+++ Mecatronica/SimulationSetup.m
@@ -1,11 +1,12 @@
 close all; clc;
+run('Simulador_DataFile.m');
 
 %% Enviorment varialbles
 % Ground Variables
-yGridVector = [0 1];
+yGridVector = [-.5 .5];
 xGridVector = [0 1.5 1.501 2 2.001 2.5 2.501 3 3.001 3.5 3.501 4 4.001 5];
 stepVector = [.015 .015; .015 .015; .015 .015; .015 .015;];
-stepType = 0;
+stepType = 1;
 switch stepType
     case 1
         zHeigths = [0 0; 0 0; stepVector; 0 0; 0 0; 0 0; 0 0; 0 0; 0 0; 0 0; 0 0;];
