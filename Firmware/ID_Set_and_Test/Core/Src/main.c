7a882df50c70aefcc6e881585726ad12c07a60fb
Yanntg <yanntgbr@gmail.com>
2022-10-05 12:05:06 -0300

    corrigidas algumas ma praticas

--- Firmware/ID_Set_and_Test/Core/Src/main.c
+++ Firmware/ID_Set_and_Test/Core/Src/main.c
@@ -86,6 +86,28 @@ static void MX_UART4_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+	uint8_t uartBuf[64];
+	uint8_t paramArray[2];  // contem os parametros de setar id
+	uint8_t paramArray2[5];  // parametros da primeira posiçao
+	uint8_t paramArray3[5];  // parametros da segunda posiçao
+	uint8_t motorId;
+
+  void sendInstruction(uint8_t instruction, uint8_t* paramArray, uint8_t numParams){
+	  //while(huartptr->gState != HAL_UART_STATE_READY);	//Tem que implementar timeout
+	  uartBuf[0] = 0xFF;				//Header
+	  uartBuf[1] = 0xFF;				//Header
+	  uartBuf[2] = motorId;				//ID
+	  uartBuf[3] = numParams + 2;		//Length
+	  uartBuf[4] = instruction;			//Instruction
+	  uint8_t i;
+	  uint8_t somaParams = 0;
+	  for(i=0; i<numParams; i++){
+		  uartBuf[i+5] = paramArray[i];	//Parameters
+		  somaParams += paramArray[i];
+	  }
+	  uartBuf[i+5] = ~(motorId + numParams + 2 + instruction + somaParams);	//Checksum
+	  HAL_UART_Transmit_DMA(&huart1, uartBuf, numParams + 6);  //tem q ficar mudando a porta aqui
+}
 
 /* USER CODE END 0 */
 
@@ -131,44 +153,25 @@ int main(void)
   MX_ADC1_Init();
   MX_UART4_Init();
   /* USER CODE BEGIN 2 */
-
-      uint8_t uartBuf[64];
-  	  uint8_t motorId = 0xFE;
-  	  uint8_t paramArray[2];
-  	  paramArray[0] = 0x03; //Starting adress da instruçao de ID
-  	  paramArray[1] = 0x00; //novo ID (se mudar tem q mudar o uartbuf2 do sendInstruction)
-  	  uint8_t paramArray2[5];
-  	  uint8_t paramArray3[5];
-
-  	  void sendInstruction(uint8_t instruction, uint8_t* paramArray, uint8_t numParams){
-  		  //while(huartptr->gState != HAL_UART_STATE_READY);	//Tem que implementar timeout
-  		  uartBuf[0] = 0xFF;				//Header
-  		  uartBuf[1] = 0xFF;				//Header
-  		  uartBuf[2] = motorId;				//ID
-  		  uartBuf[3] = numParams + 2;		//Length
-  		  uartBuf[4] = instruction;			//Instruction
-  		  uint8_t i;
-  		  uint8_t somaParams = 0;
-  		  for(i=0; i<numParams; i++){
-  			  uartBuf[i+5] = paramArray[i];	//Parameters
-  			  somaParams += paramArray[i];
-  		  }
-  		  uartBuf[i+5] = ~(motorId + numParams + 2 + instruction + somaParams);	//Checksum
-  		  HAL_UART_Transmit_DMA(&huart1, uartBuf, sizeof(uartBuf));  //tem q ficar mudando a porta aqui
-  }
-  	  //sendInstruction(0x03, paramArray, 2);  // set ID
-  	  //motorId = 00;
+  	  uint8_t novoId = 0x00;
+  	  paramArray[0] = 0x03; 	//Starting adress da instruçao de ID
+  	  paramArray[1] = novoId; 	//novo ID (se mudar tem q mudar o uartbuf2 do sendInstruction)
+  	  motorId = 0xFE;  // ID de broadcast
   	  uint16_t pos1 = 1000;
   	  uint16_t pos2 = 500;
   	  uint16_t spd = 500;
 
-  	  paramArray2[0] = 30;		//Goal position
+  	  sendInstruction(0x03, paramArray, 2);  // set ID
+
+  	  motorId = novoId;
+
+  	  paramArray2[0] = 30;			//Goal position
   	  paramArray2[1] = pos1;		//Conferir endianess
   	  paramArray2[2] = pos1>>8;
   	  paramArray2[3] = spd;
   	  paramArray2[4] = spd>>8;
 
-  	  paramArray3[0] = 30;		//Goal position
+  	  paramArray3[0] = 30;			//Goal position
   	  paramArray3[1] = pos2;		//Conferir endianess
   	  paramArray3[2] = pos2>>8;
   	  paramArray3[3] = spd;
@@ -595,7 +598,7 @@ static void MX_USART2_UART_Init(void)
 
   /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
-  huart2.Init.BaudRate = 100000;
+  huart2.Init.BaudRate = 1000000;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
   huart2.Init.StopBits = UART_STOPBITS_1;
   huart2.Init.Parity = UART_PARITY_NONE;
@@ -628,7 +631,7 @@ static void MX_USART3_UART_Init(void)
 
   /* USER CODE END USART3_Init 1 */
   huart3.Instance = USART3;
-  huart3.Init.BaudRate = 100000;
+  huart3.Init.BaudRate = 1000000;
   huart3.Init.WordLength = UART_WORDLENGTH_8B;
   huart3.Init.StopBits = UART_STOPBITS_1;
   huart3.Init.Parity = UART_PARITY_NONE;
