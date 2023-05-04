c6646ed90755876bca69a34ffb1ec1a4545a8698
YuriWit <yuridewitherran@gmail.com>
2023-05-04 12:03:48 -0300

    stash

--- Mecatronica/SimulatorSetup.m
+++ Mecatronica/SimulatorSetup.m
@@ -9,7 +9,7 @@ startHeight = 0.26;
 yGridVector = [-.5 .5];
 xGridVector = [0 1.5 1.501 2 2.001 2.5 2.501 3 3.001 3.5 3.501 4 4.001 5] - 0.2;
 stepVector = [.015 .015; .015 .015; .015 .015; .015 .015;];
-stepType = 1;
+stepType = 0;
 switch stepType
     case 1
         zHeigths = [0 0; 0 0; stepVector; 0 0; 0 0; 0 0; 0 0; 0 0; 0 0; 0 0; 0 0;];
